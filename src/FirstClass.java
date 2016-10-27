fcc72dd0c7cf2a9594ff0453915c96e3fd170b89
Kateryna <guzykevych@gmail.com>
2016-10-28 00:09:58 +0300

    Init Commit

---
+++ src/FirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by guzya on 27.10.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
