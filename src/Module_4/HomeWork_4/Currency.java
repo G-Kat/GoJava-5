4216d0bb5dce48e800d6e2956232776ff8b5bf67
Kateryna <guzykevych@gmail.com>
2016-11-20 15:59:52 +0200

    HomeWork_4_2

---
+++ src/Module_4/HomeWork_4/Currency.java
@@ -0,0 +1,14 @@
+package Module_4.HomeWork_4;
+
+/**
+ * Created by guzya on 20.11.2016.
+ */
+public enum Currency {
+    USD, EUR
+}
+
+//    Домашнее задание №4.2
+
+//        Создайте следующую структуру: Enum Currency.
+//
+//        Currency - это Enum с двумя значениями: USD, EUR.
