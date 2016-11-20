a7a1f7ead2ed376cca882fc34edb925adfa722d1
Kateryna <guzykevych@gmail.com>
2016-11-20 15:55:14 +0200

    HomeWork_4_1

---
+++ src/Module_4/HomeWork_4/ChinaBank.java
@@ -0,0 +1,32 @@
+package Module_4.HomeWork_4;
+
+import java.util.Currency;
+
+/**
+ * Created by guzya on 20.11.2016.
+ */
+public class ChinaBank extends Bank {
+    public ChinaBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        return 0;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        return 0;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        return 0;
+    }
+
+    @Override
+    int getCommission(int summ) {
+        return 0;
+    }
+}
