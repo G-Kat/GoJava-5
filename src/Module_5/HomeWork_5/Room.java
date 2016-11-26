5b07ce3f001a37d3bd63f2f32b21d0cd67f4a4df
Kateryna <guzykevych@gmail.com>
2016-11-26 16:58:39 +0200

    HomeWork_5_1

---
+++ src/Module_5/HomeWork_5/Room.java
@@ -0,0 +1,89 @@
+package Module_5.HomeWork_5;
+
+import java.util.Date;
+
+/**
+ * Created by guzya on 26.11.2016.
+ */
+public class Room {
+    private long id;
+    private int price;
+    private int persons;
+    private Date dateAvailableFrom;
+    private String hotelName;
+    private String cityName;
+
+    public Room(long id, int price, int persons, Date dateAvailableFrom, String hotelName, String cityName) {
+        this.id = id;
+        this.price = price;
+        this.persons = persons;
+        this.dateAvailableFrom = dateAvailableFrom;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPersons() {
+        return persons;
+    }
+
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+
+    public Date getDateAvailableFrom() {
+        return dateAvailableFrom;
+    }
+
+    public void setDateAvailableFrom(Date dateAvailableFrom) {
+        this.dateAvailableFrom = dateAvailableFrom;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
+// You need to create part of the System that can find hotel rooms for booking and save it to DataBase.
+// The architecture is: class Room which represent room entity that is used in system.
+// Interface API and three implementations of it. DAO interface with its implementation.
+// And finally Controller that will contain all the logic.
+// Also create Main class with main method to demonstrate results.
+//        Create UML diagrams first, and only after this write code.
+//
+//
+//        TASK 1
+//        Class Room should contain fields
+//        long id
+//        int price
+//        int persons
+//        Date dateAvailableFrom
+//        String hotelName
+//        String cityName
+//        Generate getters and setters. And create constructor with all fields.
